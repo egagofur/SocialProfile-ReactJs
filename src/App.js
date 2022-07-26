81617eb9f9328fb54029accdca095821d51d3625
egagofur <egagofurtriwahana@gmail.com>
2022-07-26 08:58:40 +0700

    ganti nomor wa

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import "./App.css";
 import { BsInstagram } from "react-icons/bs";
 import { FaTiktok } from "react-icons/fa";
 import { BsFillPersonFill } from "react-icons/bs";
-import { BsTelegram } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
 import { BsGithub } from "react-icons/bs";
 import TextLoop from "react-text-loop";
 import Typist from "react-typist";
+import Typed from "react-typed";
 
 function App() {
   const config_cursor = {
     show: true,
     blink: true,
-    element: "✨",
+    element: "",
     hideWhenDone: true,
   };
 
@@ -22,15 +23,32 @@ function App() {
   return (
     <div>
       <div className={`card ${darkmode ? "dark" : ""}`}>
-        <div className="toggle-btn" onClick={() => setdarkmode(!darkmode)}></div>
+        <div
+          className="toggle-btn"
+          onClick={() => setdarkmode(!darkmode)}
+        ></div>
         <div className="dark-mode"></div>
         <div className="card_body">
           <div className="profile text-center ">
             <img src="/logo.png" className="avatar" alt="avatar" />
             <div className="bg_content rd_12 p_8">
-              <h1>@EgaGofur</h1>
-              <p className="mt-16">Siswa SMK</p>
-              <Typist cursor={config_cursor}>Perasaan nyaman, membuatmu sulit berubah</Typist>
+              <h1>Ega(JsonDev)</h1>
+              <div className="mt-16">
+                <Typed
+                  strings={[
+                    "Student Vocational High School 1 Kepanjen",
+                    "Front-End Web Developer",
+                    "MERN Stack Dev",
+                    "UI/UX Designer",
+                  ]}
+                  backSpeed={50}
+                  typeSpeed={40}
+                  loop
+                />
+              </div>
+              <Typist className="mt-16" cursor={config_cursor}>
+                Documentation is your best friend📙
+              </Typist>
             </div>
 
             <div className="bg_content rd_12 p_8 mt-16">
@@ -40,38 +58,59 @@ function App() {
                 <span className="higthlight">CSS</span>
                 <span className="higthlight">JavaScript</span>
                 <span className="higthlight">Bootstrap 5</span>
+                <span className="higthlight">Tailwind</span>
+                <span className="higthlight">SASS</span>
+                <span className="higthlight">Github</span>
                 <span className="higthlight">React JS</span>
                 <span className="higthlight">Node JS</span>
+                <span className="higthlight">MongoDB</span>
+                <span className="higthlight">Firebase</span>
+                <span className="higthlight">Flutter</span>
                 <span className="higthlight">C++</span>
               </TextLoop>
             </div>
           </div>
           <div className="mt-16">
-            <a className="btn_action bg_content" href="https://www.instagram.com/egagofur_/">
+            <a
+              className="btn_action bg_content"
+              href="https://www.instagram.com/egagofur_/"
+            >
               <BsInstagram />
               <span>Instagram</span>
             </a>
           </div>
           <div className="mt-16">
-            <a className="btn_action bg_content" href="https://www.tiktok.com/@egagofur_?lang=en">
+            <a
+              className="btn_action bg_content"
+              href="https://www.tiktok.com/@egagofur_?lang=en"
+            >
               <FaTiktok />
               <span>Tiktok</span>
             </a>
           </div>
           <div className="mt-16">
-            <a className="btn_action bg_content" href="https://t.me/INDlalapo">
-              <BsTelegram />
-              <span>Telegram</span>
+            <a
+              className="btn_action bg_content"
+              href="https://wa.me/6281252503487?text=Hallo salam kenal"
+            >
+              <BsWhatsapp />
+              <span>Whatsapp</span>
             </a>
           </div>
           <div className="mt-16">
-            <a className="btn_action bg_content" href="https://portofolio-egagofur.vercel.app/">
+            <a
+              className="btn_action bg_content"
+              href="https://egagofur.netlify.app/"
+            >
               <BsFillPersonFill />
               <span>Portofolio</span>
             </a>
           </div>
           <div className="mt-16">
-            <a className="btn_action bg_content" href="https://github.com/egagofur">
+            <a
+              className="btn_action bg_content"
+              href="https://github.com/egagofur?tab=repositories"
+            >
               <BsGithub />
               <span>Github</span>
             </a>
